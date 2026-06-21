f81cc05728eb30e7cc23ef37f0b809742ba4d894
jeric realubit <jericrealubit@gmail.com>
2026-06-22 00:53:40 +0800

    preparing to deploy to cloudeflare

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Outputs a static build to the /out folder
+  basePath: "/rubbergem",
+  assetPrefix: "/rubbergem",
 };
 
 export default nextConfig;
